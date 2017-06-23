5bb81cfa87927b42a6394d36940c29eb367e07f4
Nakorn Sinpadung <nakornloader@gmail.com>
2017-06-23 21:31:23 +0700

    Add firebase to web.

---
+++ scripts/firebase-init.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDt8s0P86mowfGRVCJq8UqxQKFQQR2XTjk",
+    authDomain: "whitehouseengineer.firebaseapp.com",
+    databaseURL: "https://whitehouseengineer.firebaseio.com",
+    projectId: "whitehouseengineer",
+    storageBucket: "whitehouseengineer.appspot.com",
+    messagingSenderId: "748101989536"
+};
+
+firebase.initializeApp(config);
