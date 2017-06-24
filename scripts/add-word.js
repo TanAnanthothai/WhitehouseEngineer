119163f76ff79bed7888bbb43724a2e24dbc545c
Nakorn Sinpadung <nakornloader@gmail.com>
2017-06-24 11:06:18 +0700

    Add simple camera access.

--- scripts/add-word.js
+++ scripts/add-word.js
@@ -1,5 +1,6 @@
 $(function() {
   getLocation();
+  getImageFromCamera();
 });
 
 var position_latitude = '';
@@ -17,6 +18,29 @@ function submitOnClick() {
     insertData('USER01', getVocal, image_path, position_latitude, position_longitude);
   });
 
+}
+
+function getImageFromCamera() {
+  var video = document.getElementById('video');
+
+  // Get access to the camera!
+  if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      // Not adding `{ audio: true }` since we only want video now
+      navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+          video.src = window.URL.createObjectURL(stream);
+          video.play();
+      });
+  }
+
+  // Elements for taking the snapshot
+  var canvas = document.getElementById('canvas');
+  var context = canvas.getContext('2d');
+  var video = document.getElementById('video');
+
+  // Trigger photo take
+  document.getElementById("snap").addEventListener("click", function() {
+  	context.drawImage(video, 0, 0, 200, 200);
+  });
 }
 
 function getLocation() {
