ca13b329706c5806b7cfc4f89f8f56e6d9dbebee
Kriang <kriangsak.loisaeng@gmail.com>
2017-06-25 19:52:58 +0700

    try date

--- scripts/history.js
+++ scripts/history.js
@@ -1,20 +1,20 @@
-if(typeof firebase !== 'undefined'){
-    firebase.auth().onAuthStateChanged(function(user) {
-        if (user) {
-            // User is signed in.
-            window.localforage.setItem('currentUser', user.uid).then(function(){
-              console.log('uid stored locally');
-            });
-            listHistory(user.uid)
-        } else {
-            // No user is signed in.
-        }
-    });
-}else{
-  window.localforage.getItem('currentUser', function(err, currentUser){
-    if(currentUser != null){
-       listHistory(currentUser);
-    }else{
+if (typeof firebase !== 'undefined') {
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      window.localforage.setItem('currentUser', user.uid).then(function() {
+        console.log('uid stored locally');
+      });
+      listHistory(user.uid)
+    } else {
+      // No user is signed in.
+    }
+  });
+} else {
+  window.localforage.getItem('currentUser', function(err, currentUser) {
+    if (currentUser != null) {
+      listHistory(currentUser);
+    } else {
       //no active user for offline
     }
   });
@@ -36,7 +36,7 @@ var listHistory = function(userID) {
         // var datetime = convertTime(gameHistory[i]['timestamp'], '/');
         getLocat = '';
         if (typeof gameHistory[i]['latitude'] !== 'undefined' || typeof gameHistory[i]['longitude'] !== 'undefined') {
-          getLocat = '<i class="fa fa-map-marker" aria-hidden="true"></i> '+gameHistory[i]['latitude']+', '+gameHistory[i]['longitude'];
+          getLocat = '<i class="fa fa-map-marker" aria-hidden="true"></i> ' + gameHistory[i]['latitude'] + ', ' + gameHistory[i]['longitude'];
         }
         x = i + 1;
         var innerDiv = document.getElementById('container-history');
@@ -46,7 +46,7 @@ var listHistory = function(userID) {
           <h4 class="card-title">Game # ` + x + `
             <span class="mb-2 text-muted pull-right">score ` + gameHistory[i]['score'] + `/` + gameHistory[i]['fullScore'] + `</span>
           </h4>
-          <span>`+getLocat+`
+          <span>` + getLocat + `
           <span class="pull-right">` + timeSince(date) + `</span></span>
       </div></a>`;
         innerDiv.appendChild(line);
@@ -103,8 +103,13 @@ function getDuration(seconds) {
 };
 
 function timeSince(date) {
-  var seconds = Math.floor((new Date() - new Date(date*1000)) / 1000);
-  var duration = getDuration(seconds);
-  var suffix = (duration.interval > 1 || duration.interval === 0) ? 's' : '';
-  return duration.interval + ' ' + duration.epoch + suffix;
+  try {
+    var seconds = Math.floor((new Date() - new Date(date * 1000)) / 1000);
+    var duration = getDuration(seconds);
+    var suffix = (duration.interval > 1 || duration.interval === 0) ? 's' : '';
+    return duration.interval + ' ' + duration.epoch + suffix;
+  } catch (err) {
+    console.log(err);
+    return '';
+  }
 };
