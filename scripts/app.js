49cd36e4c6349ec171982ab437a52ccf6eefad0d
Touch A <gittitoucha@gmail.com>
2017-06-25 18:41:05 +0700

    remove 404.html to make service worker works correctly

--- scripts/app.js
+++ scripts/app.js
@@ -6,12 +6,14 @@
   const messaging = firebase.messaging();
 
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-     .register('/service-worker.js')
-     .then(function(registration) {
-        messaging.useServiceWorker(registration);
-        console.log('Service Worker Registered');
-        console.log(registration);
+      window.addEventListener('load', function() {
+          navigator.serviceWorker
+              .register('/service-worker.js')
+              .then(function (registration) {
+                  messaging.useServiceWorker(registration);
+                  console.log('Service Worker Registered');
+                  console.log(registration);
+              });
       });
   }
 })();
